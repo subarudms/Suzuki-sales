ef6267402f774a36b5725dc77ff89101f81c4ccd
subarudms <subarudms@gmail.com>
2026-04-15 22:43:06 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+const carData = [
+  { model: "VITARA S (Hybrid)", year: "2026", total: "60,000", detail: "發票5.5萬+配件5千" },
+  { model: "VITARA S ALLGRIP", year: "2026", total: "70,000", detail: "發票6.5萬+配件5千" },
+  { model: "SX4 GLX 2WD", year: "2025", total: "65,000", detail: "發票6萬+配件5千" },
+  { model: "SX4 GLX ALLGRIP", year: "2025", total: "75,000", detail: "發票7萬+配件5千" },
+  { model: "e-VITARA 2WD", year: "2026", total: "25,000", detail: "發票2萬+配件5千" },
+  { model: "e-VITARA 4WD", year: "2026", total: "35,000", detail: "發票3萬+配件5千" },
+  { model: "NEW-SWIFT", year: "2026", total: "40,000", detail: "發票3.5萬+配件5千, 賞車送貼紙" },
+  { model: "CARRY", year: "2025", total: "40,000", detail: "發票3.5萬+配件5千, 8888專案/0利率/送袖套" },
+  { model: "JIMNY", year: "2025", total: "40,000", detail: "發票3.5萬+配件5千, 50萬36期低利" }
+];
